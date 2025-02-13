8a4d33ef91fd460ad39d15fd28b47fc07c78f6e0
Raushan Kumar <raushan.development@gmail.com>
2025-02-13 17:53:11 +0530

    struct function & reusing instance data

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ struct User {
 }
 
 fn main() {
-    let u1 = User {
+    let mut u1 = User {
         email: String::from("raushan@email.com"),
         username: String::from("raushan"),
         active: true,
@@ -16,4 +16,28 @@ fn main() {
     println!("{}", u1.username);
     println!("{}", u1.active);
     println!("{}", u1.sign_in_count);
+
+    u1.username = String::from("raushank");
+    println!("{}", u1.username);
+
+    let u2 = build_user(String::from("r2@email.com"), String::from("r2"));
+    println!("{}", u2.email);
+
+    // reusing instance data
+    let u3 = User {
+        email: String::from("raushan3@gmail.com"),
+        username: String::from("raushan3"),
+        ..u2
+    };
+
+    println!("{}", u3.active);
+}
+
+fn build_user(email: String, username: String)-> User {
+    return User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
 }
